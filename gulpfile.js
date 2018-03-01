1c3c1fac5d31f825817e59dda5cbb4f33e7dc693
diorgejorge <diorge@gmail.com>
2018-03-01 16:30:10 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var convertEncoding = require('gulp-convert-encoding');
  
 gulp.task('default', function () {
-    return gulp.src('{folder}')
+    return gulp.src('{folder}/*')
         .pipe(convertEncoding({to: 'iso-8859-15'}))
         .pipe(gulp.dest('dist'));
 });
